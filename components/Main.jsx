c13d8cd3d09a3ed5e6ccb0af6047e0b4146d909f
Oliver Favalli <oliver.hfavalli@gmail.com>
2022-06-19 14:47:17 -0300

    feat: Categories, CategoriesItem, Hero, IconBar, LiveChannels, LiveChannelItem, Main and Navbar

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import Categories from './Categories'
+import Hero from './Hero'
+import IconBar from './IconBar'
+import LiveChannels from './LiveChannels'
 
 const Main = () => {
   return (
-    <div>Main</div>
+    <div className='absolute left-[62px] xl:left-[240px] pt-14 md:pt-9'>
+        <Hero />
+        <LiveChannels />
+        <IconBar />
+        <Categories/>
+    </div>
   )
 }
 
